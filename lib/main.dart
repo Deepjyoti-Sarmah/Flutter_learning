c34169bd2e3500faeb51b6def442d3a801706299
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2022-07-18 15:09:13 +0530

    day 1 Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:matcher/matcher.dart';
 
 void main() {
+  runApp(MaterialApp(
+    title: "Awsome App",
+    home: HomePage(),
+  ));
+}
 
-  runApp()
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Awsome App"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Hi Flutter"),
+        ),
+      ),
+    );
+  }
 }
