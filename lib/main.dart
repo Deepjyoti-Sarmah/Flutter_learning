b1202f894e909cc92ebfd81dedfda1846b8c009f
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2022-07-18 15:28:47 +0530

    Container & Text

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,39 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Awsome App"),
-      ),
-      body: Container(
-        child: Center(
-          child: Text("Hi Flutter"),
+        appBar: AppBar(
+          title: Text("Awsome App"),
         ),
-      ),
-    );
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 5,
+                    offset: Offset(2.0, 5.0),
+                  )
+                ],
+                color: Colors.teal,
+                gradient: LinearGradient(colors: [
+                  Colors.yellow,
+                  Colors.pink,
+                ])),
+            child: Text(
+              "I am a Box",
+              textAlign:TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ));
   }
 }
