7ff3485ef3f72c933c0de66a1f7a1c410b1e6083
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2022-07-21 15:17:03 +0530

    Organising Your Flutter Code

---
+++ lib/drawer/drawer.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text("Deepjyoti Sarmah"),
+            accountEmail: Text("dpjtsarmah@gmail.com"),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80"),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Account"),
+            subtitle: Text("Personal"),
+            trailing: Icon(Icons.edit),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text("Email"),
+            subtitle: Text("dpjtsarmah@gmail.com"),
+            trailing: Icon(Icons.send),
+          )
+        ],
+      ),
+    );
+  }
+}
