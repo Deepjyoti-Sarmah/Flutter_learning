7a76909f7d1d6454df3b6d443303dd624ced55f6
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2022-07-27 21:47:21 +0530

    Forms, Stack & Navigation - A Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:tutorial/pages/home_page.dart';
+// import 'package:tutorial/pages/home_page.dart';
+import 'package:tutorial/pages/login_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
       title: "Awsome App",
-      home: HomePage(),
+      home: LoginPage(),
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
+      routes: {
+        "/login": ((context) => LoginPage()),
+        "/home": ((context) => HomePage()),
+      },
     ),
   );
 }
